314dcf0b486714d3471e6f422e43afe72ba23511
Emma <alunia.moon@gmail.com>
2023-07-19 22:34:52 +0200

    Practice one show_page fn
    This function is a test to see the flow of text and user input and will be
    refactored

--- run.py
+++ run.py
@@ -2,12 +2,54 @@
     Some pseudo code to plan my game comments start with psd
 """
 
+'''game_text = [
+    {
+        "step_text": """    
+    Our furry-tail begins in a small village with two curious cats named
+    Meowshmallow and Peppurrmint who heard tales of the legendary golden fish.
+    This magical fish was said to provide an endless supply of food
+    to whomever caught it. Intrigued by the story,
+    Meowshmallow and Peppurrmint decide to embark on an adventure
+    to find this elusive fish.
+    As they set out on their journey, they come across two possible paths:""",
+        "options": [
+            "Do they follow the river upstream?",
+            "or do they venture into the dense forest?"
+        ]
+    }
+]'''
+
+# practice code:
+
+def show_page():
+    """
+        description about function
+    """
+    page_text = """Our furry-tail begins in a small village with two curious cats named
+    Meowshmallow and Peppurrmint who heard tales of the legendary golden fish.
+    This magical fish was said to provide an endless supply of food
+    to whomever caught it. Intrigued by the story,
+    Meowshmallow and Peppurrmint decide to embark on an adventure
+    to find this elusive fish.
+    As they set out on their journey, they come across two possible paths:
+    Option 1: Do they follow the river upstream?
+    Option 2: Or do they venture into the dense forest?"""
+    print(page_text)
+    user_input = input("""Write your choice (either 1 or 2):""")
+    if user_input == 1:
+        print("They follow the river (and go to step 2)")
+    else:
+        print("They go into the forest (and go to step 3)")
+
+show_page()
+
+
 
 # follow the story of Meowshmallow and Peppurrmint!
 
 # psd: add intro text to const
 # psd: print const to terminal
-print(
+'''print(
     """
     Our furry-tail begins in a small village with two curious cats named
     Meowshmallow and Peppurrmint who heard tales of the legendary golden fish.
@@ -17,7 +59,7 @@ print(
     to find this elusive fish.
     As they set out on their journey, they come across two possible paths:
     """
-)
+)'''
 
 # psd: add option 1 and 2 to consts
 # psd: offer user input to choose one option and write it to the text box
@@ -33,7 +75,7 @@ Venture into the dense forest (go to Step 3)
 
 # Step 2:
 
-print(
+'''print(
     """
     Meowshmallow and Peppurrmint follow the river upstream,
     hoping it would lead them to the golden fish. Along the way,
@@ -41,7 +83,7 @@ print(
     The turtle says to them: 'The golden fish lives in a hidden lake,
     but the path is treacherous. You must choose wisely.'
     """
-)
+)'''
 
 """
 Step 2 options 1 and 2:
@@ -51,14 +93,14 @@ Continue without the turtle's help (go to Step 5)
 
 # Step 3:
 
-print(
+'''print(
     """
     Meowshmallow and Peppurrmint venture into the dense forest,
     relying on their keen senses to guide them.
     Soon, they stumble upon a chatty squirrel who claims to know
     the whereabouts of the golden fish.
     """
-)
+)'''
 
 """
 Step 3 options 1 and 2:
@@ -68,13 +110,13 @@ Doubt the squirrel and find their own way (go to Step 7)
 
 # Step 4 with guidance:
 
-print(
+'''print(
     """
     With the turtle's guidance, Meowshmallow and Peppurrmint manage
     to find the hidden lake. As they approache, they notice two different
     spots they could possibly catch the fish from:
     """
-)
+)'''
 
 """
 Step 4 options 1 and 2:
@@ -84,13 +126,13 @@ A secluded spot beneath a willow tree (go to Step 9)
 
 # Step 5 no guidance:
 
-print(
+'''print(
     """
     Meowshmallow and Peppurrmint continue their journey without
     the turtle's help. They soon find a lake that seems promising.
     They notice two different spots they could possibly catch the fish from:
     """
-)
+)'''
 
 """
 Step 5 options 1 and 2:
@@ -100,12 +142,12 @@ A secluded spot beneath a willow tree (go to Step 11)
 
 # Step 6 trusting the squirrel:
 
-print(
+'''print(
     """
     Trusting the squirrel's directions, Meowshmallow and Peppurrmint arrive
     at the hidden lake. Looking around, they see two possible fishing spots:
     """
-)
+)'''
 
 """
 An open spot near the water's edge (go to Step 12)
@@ -114,13 +156,13 @@ A secluded spot beneath a willow tree (go to Step 13)
 
 # Step 7 doubting the squirrel:
 
-print(
+'''print(
     """
     Meowshmallow and Peppurrmint decide not to trust the squirrel and forge
     their own path. Eventually, they stumble upon a lake. They notice two
     different spots they could possibly catch the fish from:
     """
-)
+)'''
 
 """
 An open spot near the water's edge (go to Step 14)
@@ -128,85 +170,85 @@ A secluded spot beneath a willow tree (go to Step 15)
 """
 
 # Step 8 - Open Spot with Guidance
-print(
+'''print(
     """
     Choosing the open spot with the guidance of the turtle, Meowshmallow
     and Peppurrmint catch the golden fish!
     """
-)
+)'''
 
-ENDING_1 = """
+'''ENDING_1 = """
 Success: Meowshmallow and Peppurrmint return home with the golden fish.
 True to the legend, it provides them with an endless supply of food, and they
 live happily ever after.
 """
-print(ENDING_1)
+print(ENDING_1)'''
 
 # Step 9 - Secluded Spot with Guidance
-print(
+'''print(
     """
     Choosing the secluded spot, despite the turtle's guidance, leads to no
     sign of the golden fish.
     """
-)
+)'''
 
-ENDING_2 = """
+'''ENDING_2 = """
 Failure: Despite their best efforts, Meowshmallow and Peppurrmint never find
 the golden fish. They return to the farm, but the legend of the golden fish
 live on in their hearts.
 """
-print(ENDING_2)
+print(ENDING_2)'''
 
 # Step 10 - Open Spot without Guidance
-print(
+'''print(
     """
     Choosing the open spot without any guidance turned out to be a misstep,
     they find no sign of the golden fish.
     """
-)
-print(ENDING_2)
+)'''
+'''print(ENDING_2)'''
 
 # Step 11 - Secluded Spot without Guidance
-print(
+'''print(
     """
     Choosing the secluded spot without any guidance turned out to be the right
     choice! They found and caught the golden fish.
     """
-)
-print(ENDING_1)
+)'''
+'''print(ENDING_1)'''
 
 # Step 12 - Open Spot with Squirrel's Guidance
-print(
+'''print(
     """
     Choosing the open spot with the squirrel's guidance, they caught the
     golden fish!
     """
-)
-print(ENDING_1)
+)'''
+'''print(ENDING_1)'''
 
 # Step 13 - Secluded Spot with Squirrel's Guidance
-print(
+'''print(
     """
     Choosing the secluded spot, despite the squirrel's guidance, led to no
     sign of the golden fish.
     """
-)
-print(ENDING_2)
+)'''
+'''print(ENDING_2)'''
 
 # Step 14 - Open Spot without Squirrel's Guidance
-print(
+'''print(
     """
     Choosing the open spot without the squirrel's guidance led to no sign of
     the golden fish.
     """
-)
-print(ENDING_2)
+)'''
+'''print(ENDING_2)'''
 
 # Step 15 - Secluded Spot without Squirrel's Guidance
-print(
+'''print(
     """
     Choosing the secluded spot without the squirrel's guidance turned out to
     be the right choice! They found and caught the golden fish.
     """
-)
-print(ENDING_1)
+)'''
+'''print(ENDING_1)'''
