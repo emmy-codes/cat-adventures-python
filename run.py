23f00f3a06bc985aa6916d15cb0449e9052b3c4d
Emma <alunia.moon@gmail.com>
2023-07-20 15:44:08 +0200

    Finish adding game text to dict

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 """
-    Some pseudo code to plan my game comments start with psd
+    The tail of 2 curious kitties on a quest for endless fish
 """
 
 game_text = {
@@ -57,6 +57,97 @@ game_text = {
     """,
             "option_1": "step_8",
             "option_2": "step_9"
+        },
+        "step_5": {
+            "step_text":
+    """
+    Meowshmallow and Peppurrmint continue their journey without
+    the turtle's help. They soon find a lake that seems promising.
+    They notice two different spots they could possibly catch the fish from:
+    Option 1: Do they pick the open spot near the water's edge?
+    Option 2: Or do they pick the secluded spot beneath the willow tree?
+    """,
+            "option_1": "step_10",
+            "option_2": "step_11"
+        },
+        "step_6": {
+            "step_text":
+    """
+    Trusting the squirrel's directions, Meowshmallow and Peppurrmint arrive
+    at the hidden lake. Looking around, they see two possible fishing spots:
+    Option 1: Do they pick the open spot near the water's edge?
+    Option 2: Or do they pick the secluded spot beneath the willow tree?
+    """,
+            "option_1": "step_12",
+            "option_2": "step_13"
+        },
+        "step_7": {
+            "step_text":
+    """
+    Meowshmallow and Peppurrmint decide not to trust the squirrel and forge
+    their own path. Eventually, they stumble upon a lake. They notice two
+    different spots they could possibly catch the fish from:
+    Option 1: Do they pick the open spot near the water's edge?
+    Option 2: Or do they pick the secluded spot beneath the willow tree?
+    """,
+            "option_1": "step_14",
+            "option_2": "step_15"
+        },
+        "step_8": {
+            "step_text":
+    """
+    Choosing the open spot with the guidance of the turtle, 
+    Meowshmallow and Peppurrmint catch the golden fish!
+    """
+        },
+        "step_9": {
+            "step_text":
+    """
+    Choosing the secluded spot, despite the turtle's guidance, leads to no
+    sign of the golden fish.
+    """
+        },
+        "step_10":{
+            "step_text":
+    """
+    Choosing the open spot without the turtle's guidance turned out to be a misstep,
+    they find no sign of the golden fish.
+    """
+        },
+        "step_11": {
+            "step_text":
+    """
+    Choosing the secluded spot without the turtle's guidance turned out to be the right
+    choice! They found and caught the golden fish!
+    """
+        },
+        "step_12": {
+            "step_text":
+    """
+    Choosing the open spot with the squirrel's guidance, 
+    they caught the golden fish!
+    """
+        },
+        "step_13": {
+            "step_text":
+    """
+    Choosing the secluded spot, despite the squirrel's guidance, led to no
+    sign of the golden fish.
+    """
+        },
+        "step_14": {
+            "step_text":
+    """
+    Choosing the open spot without the squirrel's guidance led to no sign of
+    the golden fish.
+    """
+        },
+        "step_15": {
+            "step_text": 
+    """
+    Choosing the secluded spot without the squirrel's guidance turned out to
+    be the right choice! They found and caught the golden fish.
+    """
         }
     }
 
@@ -74,131 +165,17 @@ def show_step(step):
 
 show_step(game_text["step_1"])
 
-# Step 5 no guidance:
 
-'''print(
-    """
-    Meowshmallow and Peppurrmint continue their journey without
-    the turtle's help. They soon find a lake that seems promising.
-    They notice two different spots they could possibly catch the fish from:
-    """
-)'''
-
-"""
-Step 5 options 1 and 2:
-An open spot near the water's edge (go to Step 10)
-A secluded spot beneath a willow tree (go to Step 11)
-"""
-
-# Step 6 trusting the squirrel:
-
-'''print(
-    """
-    Trusting the squirrel's directions, Meowshmallow and Peppurrmint arrive
-    at the hidden lake. Looking around, they see two possible fishing spots:
-    """
-)'''
-
-"""
-An open spot near the water's edge (go to Step 12)
-A secluded spot beneath a willow tree (go to Step 13)
-"""
-
-# Step 7 doubting the squirrel:
-
-'''print(
-    """
-    Meowshmallow and Peppurrmint decide not to trust the squirrel and forge
-    their own path. Eventually, they stumble upon a lake. They notice two
-    different spots they could possibly catch the fish from:
-    """
-)'''
-
-"""
-An open spot near the water's edge (go to Step 14)
-A secluded spot beneath a willow tree (go to Step 15)
-"""
-
-# Step 8 - Open Spot with Guidance
-'''print(
-    """
-    Choosing the open spot with the guidance of the turtle, Meowshmallow
-    and Peppurrmint catch the golden fish!
-    """
-)'''
-
-'''ENDING_1 = """
+# End text for success
+ENDING_1 = """
 Success: Meowshmallow and Peppurrmint return home with the golden fish.
 True to the legend, it provides them with an endless supply of food, and they
 live happily ever after.
 """
-print(ENDING_1)'''
 
-# Step 9 - Secluded Spot with Guidance
-'''print(
-    """
-    Choosing the secluded spot, despite the turtle's guidance, leads to no
-    sign of the golden fish.
-    """
-)'''
-
-'''ENDING_2 = """
+# End text for failure
+ENDING_2 = """
 Failure: Despite their best efforts, Meowshmallow and Peppurrmint never find
 the golden fish. They return to the farm, but the legend of the golden fish
 live on in their hearts.
 """
-print(ENDING_2)'''
-
-# Step 10 - Open Spot without Guidance
-'''print(
-    """
-    Choosing the open spot without any guidance turned out to be a misstep,
-    they find no sign of the golden fish.
-    """
-)'''
-'''print(ENDING_2)'''
-
-# Step 11 - Secluded Spot without Guidance
-'''print(
-    """
-    Choosing the secluded spot without any guidance turned out to be the right
-    choice! They found and caught the golden fish.
-    """
-)'''
-'''print(ENDING_1)'''
-
-# Step 12 - Open Spot with Squirrel's Guidance
-'''print(
-    """
-    Choosing the open spot with the squirrel's guidance, they caught the
-    golden fish!
-    """
-)'''
-'''print(ENDING_1)'''
-
-# Step 13 - Secluded Spot with Squirrel's Guidance
-'''print(
-    """
-    Choosing the secluded spot, despite the squirrel's guidance, led to no
-    sign of the golden fish.
-    """
-)'''
-'''print(ENDING_2)'''
-
-# Step 14 - Open Spot without Squirrel's Guidance
-'''print(
-    """
-    Choosing the open spot without the squirrel's guidance led to no sign of
-    the golden fish.
-    """
-)'''
-'''print(ENDING_2)'''
-
-# Step 15 - Secluded Spot without Squirrel's Guidance
-'''print(
-    """
-    Choosing the secluded spot without the squirrel's guidance turned out to
-    be the right choice! They found and caught the golden fish.
-    """
-)'''
-'''print(ENDING_1)'''
