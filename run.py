39897ab0fc076eca996020759d0c13618250057b
Emma <alunia.moon@gmail.com>
2023-07-21 11:22:05 +0200

    Update game visual
    Add some "style" to the dict to give better readability to
    the game

--- run.py
+++ run.py
@@ -5,169 +5,199 @@
 game_text = {
         "step_1": {
             "step_text":
-    """
-    Our furry-tail begins in a small village with two curious cats named
-    Meowshmallow and Peppurrmint who heard tales of the legendary golden fish.
-    This magical fish was said to provide an endless supply of food
-    to whomever caught it. Intrigued by the story, they decide to embark 
-    on an adventure to find this elusive fish.\n
-    As they set out on their journey, they come across two possible paths:
-    Option 1: Do they follow the river upstream?
-    Option 2: Or do they venture into the dense forest?
-    """,
+"""
+$ -------------------------------------------------------------------------- $
+$ Our furry-tail begins in a small village with two curious cats named       $
+$ Meowshmallow and Peppurrmint who heard tales of a legendary golden fish.   $
+$ This magical fish was said to provide an endless supply of food            $
+$ to whomever caught it. Intrigued by the story, they decide to embark       $
+$ on an adventure to find this elusive fish.                                 $
+$ -------------------------------------------------------------------------- $
+$ As they set out on their journey, they come across two possible paths:     $
+$ Option 1: Do they follow the river upstream?                               $
+$ Option 2: Or do they venture into the dense forest?                        $
+""",
             "option_1": "step_2", 
             "option_2": "step_3",
             "end_game": False
         },
         "step_2": {
             "step_text": 
-    """
-    Meowshmallow and Peppurrmint follow the river upstream,
-    hoping it would lead them to the golden fish. Along the way,
-    they discover a wise old turtle who offers them advice.
-    The turtle says to them: 'The golden fish lives in a hidden lake,
-    but the path is treacherous. You must choose wisely.'\n
-    Option 1: Do they ask the turtle for guidance?
-    Option 2: Or do they continue without the turtle's help?
-    """,
+"""
+$ -------------------------------------------------------------------------- $
+$ Meowshmallow and Peppurrmint follow the river upstream,                    $
+$ hoping it would lead them to the golden fish. Along the way,               $
+$ they discover a wise old turtle who offers them advice.                    $
+$ The turtle says to them: 'The golden fish lives in a hidden lake,          $
+$ but the path is treacherous. You must choose wisely.'                      $
+$ -------------------------------------------------------------------------- $
+$ Option 1: Do they ask the turtle for guidance?                             $
+$ Option 2: Or do they continue without the turtle's help?                   $
+""",
             "option_1": "step_4", 
             "option_2": "step_5",
             "end_game": False
         },
         "step_3": {
             "step_text":
-    """
-    Meowshmallow and Peppurrmint venture into the dense forest,
-    relying on their keen senses to guide them.
-    Soon, they stumble upon a chatty squirrel who claims to know
-    the whereabouts of the golden fish.\n
-    Option 1: Do they trust the squirrel and follow its directions?
-    Option 2: Or do they doubt the squirrel and find their own way?
-    """,
+"""
+$ -------------------------------------------------------------------------- $
+$ Meowshmallow and Peppurrmint venture into the dense forest,                $
+$ relying on their keen senses to guide them.                                $
+$ Soon, they stumble upon a chatty squirrel who claims to know               $
+$ the whereabouts of the golden fish.                                        $
+$ -------------------------------------------------------------------------- $
+$ Option 1: Do they trust the squirrel and follow its directions?            $
+$ Option 2: Or do they doubt the squirrel and find their own way?            $
+""",
             "option_1": "step_6",
             "option_2": "step_7",
             "end_game": False
         },
         "step_4": {
             "step_text":
-    """
-    With the turtle's guidance, Meowshmallow and Peppurrmint manage
-    to find the hidden lake. As they approache, they notice two different
-    spots they could possibly catch the fish from:\n
-    Option 1: Do they pick the open spot near the water's edge?
-    Option 2: Or do they pick the secluded spot beneath the willow tree?
-    """,
+"""
+$ -------------------------------------------------------------------------- $
+$ With the turtle's guidance, Meowshmallow and Peppurrmint manage            $
+$ to find the hidden lake. As they approache, they notice two different      $
+$ spots they could possibly catch the fish from:                             $
+$ -------------------------------------------------------------------------- $
+$ Option 1: Do they pick the open spot near the water's edge?                $
+$ Option 2: Or do they pick the secluded spot beneath the willow tree?       $
+""",
             "option_1": "step_8",
             "option_2": "step_9",
             "end_game": False
         },
         "step_5": {
             "step_text":
-    """
-    Meowshmallow and Peppurrmint continue their journey without
-    the turtle's help. They soon find a lake that seems promising.
-    They notice two different spots they could possibly catch the fish from:\n
-    Option 1: Do they pick the open spot near the water's edge?
-    Option 2: Or do they pick the secluded spot beneath the willow tree?
-    """,
+"""
+$ -------------------------------------------------------------------------- $
+$ Meowshmallow and Peppurrmint continue their journey without                $
+$ the turtle's help. They soon find a lake that seems promising.             $
+$ They notice two different spots they could possibly catch the fish from:   $
+$ -------------------------------------------------------------------------- $
+$ Option 1: Do they pick the open spot near the water's edge?                $
+$ Option 2: Or do they pick the secluded spot beneath the willow tree?       $
+""",
             "option_1": "step_10",
             "option_2": "step_11",
             "end_game": False
         },
         "step_6": {
             "step_text":
-    """
-    Trusting the squirrel's directions, Meowshmallow and Peppurrmint arrive
-    at the hidden lake. Looking around, they see two possible fishing spots:\n
-    Option 1: Do they pick the open spot near the water's edge?
-    Option 2: Or do they pick the secluded spot beneath the willow tree?
-    """,
+"""
+$ -------------------------------------------------------------------------- $
+$ Trusting the squirrel's directions, Meowshmallow and Peppurrmint arrive    $
+$ at the hidden lake. Looking around, they see two possible fishing spots:   $
+$ -------------------------------------------------------------------------- $
+$ Option 1: Do they pick the open spot near the water's edge?                $
+$ Option 2: Or do they pick the secluded spot beneath the willow tree?       $
+""",
             "option_1": "step_12",
             "option_2": "step_13",
             "end_game": False
         },
         "step_7": {
             "step_text":
-    """
-    Meowshmallow and Peppurrmint decide not to trust the squirrel and forge
-    their own path. Eventually, they stumble upon a lake. They notice two
-    different spots they could possibly catch the fish from:\n
-    Option 1: Do they pick the open spot near the water's edge?
-    Option 2: Or do they pick the secluded spot beneath the willow tree?
-    """,
+"""
+$ -------------------------------------------------------------------------- $
+$ Meowshmallow and Peppurrmint decide not to trust the squirrel and forge    $
+$ their own path. Eventually, they stumble upon a lake. They notice two      $
+$ different spots they could possibly catch the fish from:                   $
+$ -------------------------------------------------------------------------- $
+$ Option 1: Do they pick the open spot near the water's edge?                $
+$ Option 2: Or do they pick the secluded spot beneath the willow tree?       $
+""",
             "option_1": "step_14",
             "option_2": "step_15",
             "end_game": False
         },
         "step_8": {
             "step_text":
-    """
-    Choosing the open spot with the guidance of the turtle, 
-    Meowshmallow and Peppurrmint catch the golden fish!
-    """,
+"""
+$ -------------------------------------------------------------------------- $
+$ Choosing the open spot with the guidance of the turtle,                    $
+$ Meowshmallow and Peppurrmint catch the golden fish!                        $
+$ -------------------------------------------------------------------------- $
+""",
             "end_game": True,
             "success": True
         },
         "step_9": {
             "step_text":
-    """
-    Choosing the secluded spot, despite the turtle's guidance, leads to no
-    sign of the golden fish.
-    """,
+"""
+$ -------------------------------------------------------------------------- $
+$ Choosing the secluded spot, despite the turtle's guidance, leads to no     $
+$ sign of the golden fish.                                                   $
+$ -------------------------------------------------------------------------- $
+""",
             "end_game": True,
             "success": False
         },
         "step_10":{
             "step_text":
-    """
-    Choosing the open spot without the turtle's guidance turned out to be a 
-    misstep, as they find no sign of the golden fish.
-    """,
+"""
+$ -------------------------------------------------------------------------- $
+$ Choosing the open spot without the turtle's guidance turned out to be a    $
+$ misstep, as they find no sign of the golden fish.                          $
+$ -------------------------------------------------------------------------- $
+""",
             "end_game": True,
             "success": False
         },
         "step_11": {
             "step_text":
-    """
-    Choosing the secluded spot without the turtle's guidance turned out to be the right
-    choice! They found and caught the golden fish!
-    """,
+"""
+$ -------------------------------------------------------------------------- $
+$ Choosing the secluded spot without the turtle's guidance turned out to be  $
+$ the right choice! They found and caught the golden fish!                   $
+$ -------------------------------------------------------------------------- $
+""",
             "end_game": True,
             "success": True
         },
         "step_12": {
             "step_text":
-    """
-    Choosing the open spot with the squirrel's guidance, 
-    they caught the golden fish!
-    """,
+"""
+$ -------------------------------------------------------------------------- $
+$ Choosing the open spot with the squirrel's guidance,                       $
+$ they caught the golden fish!                                               $
+$ -------------------------------------------------------------------------- $
+""",
             "end_game": True,
             "success": True
         },
         "step_13": {
             "step_text":
-    """
-    Choosing the secluded spot, despite the squirrel's guidance, led to no
-    sign of the golden fish.
-    """,
+"""
+$ -------------------------------------------------------------------------- $
+$ Choosing the secluded spot, despite the squirrel's guidance,               $
+$ led to no sign of the golden fish.                                         $
+$ -------------------------------------------------------------------------- $
+""",
             "end_game": True,
             "success": False
         },
         "step_14": {
             "step_text":
-    """
-    Choosing the open spot without the squirrel's guidance led to no sign of
-    the golden fish.
-    """,
+"""
+$ -------------------------------------------------------------------------- $
+$ Choosing the open spot without the squirrel's guidance                     $
+$ led to no sign of the golden fish.                                         $
+$ -------------------------------------------------------------------------- $
+""",
             "end_game": True,
             "success": False
         },
         "step_15": {
             "step_text": 
-    """
-    Choosing the secluded spot without the squirrel's guidance turned out to
-    be the right choice! They found and caught the golden fish.
-    """,
+"""
+$ -------------------------------------------------------------------------- $
+$ Choosing the secluded spot without the squirrel's guidance turned out to   $
+$ be the right choice! They found and caught the golden fish.                $
+$ -------------------------------------------------------------------------- $
+""",
             "end_game": True,
             "success": True
         }
@@ -175,15 +205,19 @@ game_text = {
 
 # End text for success
 ENDING_1 = """
-Meowshmallow and Peppurrmint return home with the golden fish.
-True to the legend, it provides them with an endless supply of food, and they
-live happily ever after.
+$ -------------------------------------------------------------------------- $
+$ Meowshmallow and Peppurrmint return home with the golden fish.             $
+$ True to the legend, it provides them with an endless supply of food,       $
+$ and they live happily ever after.                                          $
+$ -------------------------------------------------------------------------- $
 """
 # End text for failure
 ENDING_2 = """
-Despite their best efforts, Meowshmallow and Peppurrmint never find
-the golden fish. They return to the farm, but the legend of the golden fish
-live on in their hearts.
+$ -------------------------------------------------------------------------- $
+$ Despite their best efforts, Meowshmallow and Peppurrmint never find        $
+$ the golden fish. They return to the farm, but the legend                   $
+$ of the golden fish live on in their hearts.                                $
+$ -------------------------------------------------------------------------- $
 """
 
 def start_game():
@@ -191,15 +225,25 @@ def start_game():
         Game start options
     """
     print("""
-Welcome to the adventures of Meowshmallow and Peppurrmint,
-two very curious cats that are on a mission!\n
-Are you ready to start the game?""")
+$ -------------------------------------------------------------------------- $
+$ Welcome to the adventures of Meowshmallow and Peppurrmint,                 $
+$ two very curious cats that are on a mission!                               $
+$ -------------------------------------------------------------------------- $
+$                                                                            $
+$ Are you ready to start the game?                                           $
+$ -------------------------------------------------------------------------- $
+"""
+)
     game_time = input("Type 1 for yes, or 2 for no: ")
     if game_time == "1":
         show_step(game_text["step_1"])
     elif game_time == "2":
-        while game_time is not "1":
-            print("Are you sure? Let's take the kitties on an adventure!")
+        while game_time != "1":
+            print("""
+$ -------------------------------------------------------------------------- $
+$ Are you sure? Let's take the kitties on an adventure!                      $
+$ -------------------------------------------------------------------------- $
+""")
             game_time = input("Type 1 for yes, or 2 for no: ")
         show_step(game_text["step_1"])
     else:
@@ -244,6 +288,3 @@ def show_step(step):
         print("The cats look around, wondering what to do next")
 
 start_game()
-"""
-show_step(game_text["step_1"])
-"""
